23b64b59b96ded4712f7c90dc9f4b10424100f68
Urs Behrmann <urs.behrmann@digitel.swiss>
2024-11-15 08:34:11 +0100

    init

---
+++ hps_timer_irq/soft/src/app_timer_irq.c
@@ -0,0 +1,38 @@
+/*****************************************************************************************
+ * HEIG-VD
+ * Haute Ecole d'Ingenerie et de Gestion du Canton de Vaud
+ * School of Business and Engineering in Canton de Vaud
+ *****************************************************************************************
+ * REDS Institute
+ * Reconfigurable Embedded Digital Systems
+ *****************************************************************************************
+ *
+ * File                 : app_timer_irq.c
+ * Author               : Anthony Convers
+ * Date                 : 27.10.2022
+ *
+ * Context              : ARE lab
+ *
+ *****************************************************************************************
+ * Brief: Using timer IRQ on DE1-SoC board
+ *
+ *****************************************************************************************
+ * Modifications :
+ * Ver    Date        Student      Comments
+ * 0.0    27.10.2022  ACS           Initial version.
+ *
+*****************************************************************************************/
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "axi_lw.h"
+
+int __auto_semihosting;
+
+int main(void){
+
+    printf("Laboratoire: Timer IRQ \n");
+    
+    // TO BE COMPLETE
+
+}
